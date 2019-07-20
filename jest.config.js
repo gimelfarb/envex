15605322f3a71cc34943434c9d6964575a47225f
Lev Gimelfarb <lev@gimelfarb.net>
2019-07-20 13:58:05 +1200

    test: 💍 Ignore .test.js from examples folder

--- jest.config.js
+++ jest.config.js
@@ -145,9 +145,10 @@ module.exports = {
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: [
+     "/node_modules/",
+     "/examples/"
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
